513a90f6cda5cbe2ad999f16d216ad0410da0a2b
MandraVEVO <paratrabajosquesenecesiten15@gmail.com>
2025-12-29 06:15:03 -0600

    implementacion de end points basicos para la comunicacion con nats

---
+++ src/config/envs.ts
@@ -0,0 +1,39 @@
+import 'dotenv/config';
+import * as joi from 'joi';
+
+interface EnvVars {
+        PORT: number;
+
+        
+        NATS_SERVERS: string[];
+        
+}
+
+const envsSchema = joi.object({
+    PORT: joi.number().required(),
+
+    NATS_SERVERS: joi.array().items(joi.string()).required(),///validar nats servers
+   
+})
+.unknown(true);
+
+const{error, value} = envsSchema.validate(
+    {
+        ...process.env,
+        NATS_SERVERS: process.env.NATS_SERVERS?.split(','), //para separar los servidores ya que estan en arreglo
+    }
+);
+
+if (error) {
+    throw new Error(`Config validation error: ${error.message}`);
+}
+
+const envVars: EnvVars = value; 
+
+
+export const envs= {
+    port: envVars.PORT,
+    
+    natsServers: envVars.NATS_SERVERS,
+   
+}
