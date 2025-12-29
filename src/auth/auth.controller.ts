663ef18341d7c4ad6585f82011b8989487323d55
MandraVEVO <paratrabajosquesenecesiten15@gmail.com>
2025-12-29 07:00:42 -0600

    implementacion de DTO para auth y payload de informacion para ver el funcionamiento de estos

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Controller } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { MessagePattern } from '@nestjs/microservices';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+import { LoginUserDto, RegisterUserDto } from './dto';
 
 @Controller()
 export class AuthController {
@@ -9,13 +10,13 @@ export class AuthController {
 
 
   @MessagePattern('auth.register.user')
-  registerUser(){
-    return 'register user';
+  registerUser(@Payload() registerUserDto: RegisterUserDto){
+    return registerUserDto;
   }
 
   @MessagePattern('auth.login.user')
-  loginUser(){
-    return 'login user';
+  loginUser(@Payload() loginUserDto: LoginUserDto){
+    return loginUserDto;
   }
 
   @MessagePattern('auth.verify.token')
